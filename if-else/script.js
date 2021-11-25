d5155bfeada1f3d7a77cfa87173abfb873a4b3ec
moehzi <faizi1234098@gmail.com>
2021-11-25 21:26:24 +0800

    hw: if-else

---
+++ if-else/script.js
@@ -0,0 +1,19 @@
+var nilai = 8;
+
+if (nilai > 7) {
+  hasil = "Belajar";
+} else if (nilai == 12) {
+  hasil = "Makan";
+} else if (nilai >= 5) {
+  hasil = "Mandi";
+} else if (nilai <= 2) {
+  hasil = "Jogging";
+} else if (nilai > 3) {
+  hasil = "Olahraga";
+} else if (nilai < 10) {
+  hasil = "Main";
+} else {
+  hasil = "Tidur";
+}
+
+console.log(hasil);
